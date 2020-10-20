3a972d798f4f40a3984dde0c9508e93aca747bb4
geraldy1st <geraldy.leondas@gmail.com>
2020-10-20 12:56:34 +0200

    maj about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,8 @@ const About = () => {
       <h1>About this app</h1>
       <p>App to search Github users</p>
       <p>Version 1.0.1</p>
+      <p><a href="https://github.com/geraldy1st">Copyright © 2019 Social Network by Geraldy. All rights reserved.</a></p>
+      
     </Fragment>
   );
 };
